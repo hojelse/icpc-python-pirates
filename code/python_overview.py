68a9b652421d717ca956f5a2dc81a4f74f60edde
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-11-19 22:09:42 +0100

    Updates from Overleaf

---
+++ code/python_overview.py
@@ -0,0 +1,18 @@
+
+# Graph representation: nested dict
+# weighted => use values for weights
+# unweighted => use dummy values
+# same repr, same code, same algorithms
+# also easily represent dir/undir
+
+unweighted = """
+    0 1
+    0 2
+    0 3
+    1 2
+    1 3
+    2 3
+"""
+
+
+
